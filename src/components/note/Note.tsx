cb6ed324cfeb8c33ee7b24f53b6b331ec23307b7
Miko Barreyro <110441796+barreyrojohnmiko@users.noreply.github.com>
2023-04-24 14:02:32 +0800

    fixed initial note date and characters count bug

--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -24,36 +24,57 @@ const Note = () => {
     dispatch(setModeStatus("edit"));
   };
 
-useEffect(() => {
-  if (!timestamp) {
-    const storedTimestamp = localStorage.getItem("GUID");
-    if (storedTimestamp) {
-      dispatch(setTimestamp(storedTimestamp));
-    } else {
-      const newTimestamp = new Date().toISOString();
-      dispatch(setTimestamp(newTimestamp));
+  useEffect(() => {
+    if (!timestamp) {
+      const storedTimestamp = localStorage.getItem("GUID");
+      if (storedTimestamp) {
+        dispatch(setTimestamp(storedTimestamp));
+      } else {
+        const newTimestamp = new Date().toISOString();
+        dispatch(setTimestamp(newTimestamp));
+      }
     }
-  }
+  }, [timestamp, dispatch]);
 
-  const allNotesData = localStorage.getItem('allNotesData');
-  if (allNotesData) {
-    const parsedNotesData = JSON.parse(allNotesData);
-    if (parsedNotesData === null) {
-      localStorage.setItem('allNotesData', JSON.stringify([{ "titleText": "Greetings!", "contentText": "Welcome!\n\nPlease use the add note button located at the bottom right of the screen to start adding notes.\n\nIf notes are already existing, you can just import the .JSON file using the three-dots buttons at upper right part of the screen.", "GUID": timestamp, "charactersCount": 3 }]));
-      window.location.reload();
+  useEffect(() => {
+    if (timestamp) {
+      const allNotesData = localStorage.getItem("allNotesData");
+      if (allNotesData) {
+        const parsedNotesData = JSON.parse(allNotesData);
+        if (parsedNotesData === null) {
+          const defaultNote = {
+            titleText: "Greetings!",
+            contentText: "Welcome!\n\nPlease use the add note button located at the bottom right of the screen to start adding notes.\n\nIf notes are already existing, you can just import the .JSON file using the three-dots buttons at upper right part of the screen.",
+            GUID: timestamp,
+            charactersCount: 0,
+          };
+          defaultNote.charactersCount = defaultNote.contentText.replace(/\s+/g, "").length;
+          localStorage.setItem("allNotesData", JSON.stringify([defaultNote]));
+          window.location.reload();
+        }
+      } else {
+        const defaultNote = {
+          titleText: "Greetings!",
+          contentText: "Welcome!\n\nPlease use the add note button located at the bottom right of the screen to start adding notes.\n\nIf notes are already existing, you can just import the .JSON file using the three-dots buttons at upper right part of the screen.",
+          GUID: timestamp,
+          charactersCount: 0,
+        };
+        defaultNote.charactersCount = defaultNote.contentText.replace(/\s+/g, "").length;
+        localStorage.setItem("allNotesData", JSON.stringify([defaultNote]));
+        window.location.reload();
+      }
     }
-  } 
-  else {
-    localStorage.setItem('allNotesData', JSON.stringify([{ "titleText": "Greetings!", "contentText": "Welcome!\n\nPlease use the add note button located at the bottom right of the screen to start adding notes.\n\nIf notes are already existing, you can just import the .JSON file using the three-dots buttons at upper right part of the screen.", "GUID": timestamp, "charactersCount": 3 }]));
-    window.location.reload();
-  }
-}, [timestamp, dispatch]);
+  }, [timestamp]);
 
   return (
     <div className="note-main-container">
       <div className="note-flex-container">
         {filteredNotes.map((data: any, index: number) => (
-          <div key={index} className="note-panel-container" onClick={() => handleOpenNotePanel(data)}>
+          <div
+            key={index}
+            className="note-panel-container"
+            onClick={() => handleOpenNotePanel(data)}
+          >
             <NotePanel storedData={data} />
           </div>
         ))}
